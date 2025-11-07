2095ca0c2f5a71f0853d632c681cd93435380950
ftiffanyy <ftyffany@student.ciputra.ac.id>
2025-11-07 11:59:36 +0700

    picker

--- w09/ContentView.swift
+++ w09/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isChecked = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Picker")
         }
         .padding()
     }
