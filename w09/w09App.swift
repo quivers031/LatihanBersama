15cf8dfc609763a0a7d131d8fc64b1cd646753d2
quivers031 <quivers031@gmail.com>
2025-11-07 11:12:19 +0700

    Initial Commit

---
+++ w09/w09App.swift
@@ -0,0 +1,17 @@
+//
+//  w09App.swift
+//  w09
+//
+//  Created by student on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct w09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
